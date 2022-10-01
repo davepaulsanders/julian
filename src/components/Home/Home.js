f7b8e356a87a516c541f7e6b2caa4e1762044551
Dave Sanders <davepaulsanders@gmail.com>
2022-10-01 19:11:45 -0400

    Adding video to landing page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Home.css";
-
+const tavern = require('../../assets/tavernVideo.mp4')
 export const Home = () => {
-  return <div className="home w-100"></div>;
+  return <div className="home w-100 flex justify-center md:justify-end items-center pb-40">
+    <video autoPlay loop muted playsInline className="video mt-40 md:mt-0 mr-0 md:mr-40" src={tavern}></video>
+
+  </div>;
 };
