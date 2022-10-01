cc8e98e3b3fd7c91b34c93258733c522210f6cf2
Dave Sanders <davepaulsanders@gmail.com>
2022-10-01 18:16:38 -0400

    Fixed overflowing text by removing rules under * and putting them into html, created responsive grid for photos

--- src/components/Photos/Photos.js
+++ src/components/Photos/Photos.js
@@ -1,31 +1,48 @@
 import React from "react";
-const bio = require("../../assets/bio.jpg");
-const bio2 = require("../../assets/bio2.jpg");
-const bio3 = require("../../assets/bio3.jpg");
+import "./Photos.css"
+const photos1 = require("../../assets/photos1.jpg");
+const photos2 = require("../../assets/photos2.jpg");
+const photos3 = require("../../assets/photos3.png");
+const photos4 = require("../../assets/photos4.jpg");
+const photos5 = require("../../assets/photos5.jpg");
+const photos6 = require("../../assets/photos6.png");
 
 export const Photos = () => {
   return (
-    <div className="grid grid-cols-12 w-full gap-2 grid-rows-4 bg-gray-500 pt-10">
-      <img
-        className="col-start-3 col-end-7 row-start-1 row-span-2"
-        src={bio}
-        alt="Julian Berkowitz"
-      />
-      <img
-        className="col-start-7 col-end-9 row-start-1 row-span-1"
-        src={bio2}
-        alt="Julian Berkowitz"
-      />
-      <img
-        className="col-start-7 col-end-9 row-start-2 row-span-1"
-        src={bio3}
-        alt="Julian Berkowitz"
-      />
-      <img
-        className="col-start-9 col-end-12 row-start-1 row-span-2 self-center"
-        src={bio}
-        alt="Julian Berkowitz"
-      />
+    <div className="bg-gray-500 py-10 flex flex-col items-center">
+    <h2 className="photos-title text-6xl mb-10">PHOTOS</h2>
+      <div className="grid md:grid-cols-8 lg:grid-cols-12 w-full md:w-11/12 lg:w-3/4 gap-1 md:gap-2 md:grid-rows-3 lg:grid-rows-2">
+        <img
+          className="col-start-1 md:col-end-5 lg:col-end-5"
+          src={photos1}
+          alt="Julian Berkowitz"
+        />
+        <img
+          className="md:col-start-5 md:col-end-9"
+          src={photos4}
+          alt="Julian Berkowitz"
+        />
+        <img
+          className="md:col-start-1 md:row-start-2 md:col-end-5 lg:row-start-1 lg:col-start-9 lg:col-end-13"
+          src={photos2}
+          alt="Julian Berkowitz"
+        />
+        <img
+          className="md:col-start-5 md:row-start-2 md:col-end-9"
+          src={photos3}
+          alt="Julian Berkowitz"
+        />
+        <img
+          className="md:col-start-1 md:row-start-3 md:col-end-5 lg:row-start-2"
+          src={photos5}
+          alt="Julian Berkowitz"
+        />
+        <img
+          className="md:col-start-5 md:row-start-3 md:col-end-9 lg:row-start-2 lg:col-start-9 lg:col-end-13"
+          src={photos6}
+          alt="Julian Berkowitz"
+        />
+      </div>
     </div>
   );
 };
