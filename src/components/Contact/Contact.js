47b39521a802a6a5defa152336bd492459487c59
Dave Sanders <davepaulsanders@gmail.com>
2022-11-16 15:16:25 -0500

    Hiding emailjs keys

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,15 +1,23 @@
 import React, { useRef } from "react";
 import "./Contact.css";
-
+import emailjs from "@emailjs/browser";
+import { serviceKey, publicKey } from "../../keys";
 export const Contact = () => {
-  const emailRef = useRef();
-  const messageRef = useRef();
+  const form = useRef();
 
-  const handleFormSubmit = async (e) => {
+  const handleFormSubmit = (e) => {
     e.preventDefault();
-    
-    
-    
+
+    emailjs
+      .sendForm(serviceKey, "template_upsya8q", form.current, publicKey)
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
 
   return (
@@ -21,14 +29,18 @@ export const Contact = () => {
         <h2 className="section-title contact-title text-slate-200 text-5xl pb-10 pl-4 self-start relative">
           CONTACT
         </h2>
-        <form className="flex flex-col w-11/12 md:w-8/12 xl:w-6/12">
+        <form
+          onSubmit={handleFormSubmit}
+          ref={form}
+          className="flex flex-col w-11/12 md:w-8/12 xl:w-6/12"
+        >
           <label className="text-xl text-left text-slate-200" htmlFor="email">
             Email
           </label>
           <input
             type="text"
             id="email"
-            ref={emailRef}
+            name="email"
             className="mb-2 p-2 shadow-inner"
           />
           <label className="text-xl text-left text-slate-200" htmlFor="message">
@@ -37,11 +49,11 @@ export const Contact = () => {
           <textarea
             id="message"
             rows="5"
-            ref={messageRef}
+            name="message"
             className="mb-2 p-2 shadow-inner"
           ></textarea>
           <button
-            onClick={handleFormSubmit}
+            type="submit"
             className="details-button border-2 leading-5 text-slate-200 py-2 border-slate-200 w-full mt-2 text-xl hover:text-black hover:bg-slate-200"
           >
             Send
