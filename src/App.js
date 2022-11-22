cc50a04b3a738373db6b4eec9f0b66426d805a5f
Dave Sanders <davepaulsanders@gmail.com>
2022-11-22 14:25:14 -0500

    Adding basename to router

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App flex flex-col items-center">
       <Header />
-      <Router>
+      <Router basename={"https://helpful-capybara-215f21.netlify.app"}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/bio" element={<Bio />} />
